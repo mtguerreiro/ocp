54f47af98939711ec178243cf1f4f82de938d980
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2025-10-25 19:12:15 +0200

    Adding dependencies to python package

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-setup(name='pyocp',
-      version='0.1.0',
-      description='Open Controller Project',
-      #url='',
-      author='LRS',
-      author_email='marcotulio.guerreiro@gmail.com',
-      packages=['pyocp'],
-      )
+setup(
+    name='pyocp',
+    version='0.1.0',
+    description='Open Controller Project',
+    #url='',
+    author='LRS',
+    author_email='marcotulio.guerreiro@gmail.com',
+    packages=['pyocp'],
+    install_requires=[
+      'numpy',
+      'scipy',
+      'matplotlib',
+    ],    
+)
